3d63fec57eb184d5701a29459596dbc606469d65
Gábor Polgár <56192725+gaborpolgar@users.noreply.github.com>
2020-11-02 22:42:06 +0100

    Product task-draft commmited.

---
+++ src/main/java/classstructureintegrate/Product.java
@@ -0,0 +1,26 @@
+package classstructureintegrate;
+
+public class Product {
+
+    private String name;
+    private int price;
+
+    public String getString() {
+        return String;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public Product(String name, int price) {
+    }
+
+    public void increasePrice(int){
+        }
+
+        public void decreasePrice(int){
+        }
+
+    }
+
