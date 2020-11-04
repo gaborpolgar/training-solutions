62b7eaf20c41b69712e843ce5a2abdcb97a483e3
Gábor Polgár <56192725+gaborpolgar@users.noreply.github.com>
2020-11-04 22:20:47 +0100

    committed.

---
+++ src/main/java/localvariables/LocalVariablesMain.java
@@ -0,0 +1,20 @@
+package localvariables;
+
+public class LocalVariablesMain {
+
+    public static void main(String[] args) {
+        boolean b = false;
+        System.out.println(b);
+        int a = 2;
+        int i = 3;
+        int j = 4;
+        int k = i;
+        String s = "Hello World";
+        String t = s;
+
+    }
+
+    public void testMethod() {
+        int x = 0;
+    }
+}
