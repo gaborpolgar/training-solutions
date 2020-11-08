46a038cd4763125caedcaf909b1b97c198e50cf6
Gábor Polgár <56192725+gaborpolgar@users.noreply.github.com>
2020-11-08 22:11:05 +0100

    Schema committed, ending later.

---
+++ src/main/java/week02/Controller.java
@@ -0,0 +1,25 @@
+package week02;
+
+public class Controller {
+
+        Office office;
+
+    public static void main(String[] args) {
+        Office office = new Office();
+        office.
+        public void readOffice(){
+            Office office = new Office();
+
+        }
+
+        public void printMenu(){
+            System.out.println("");
+
+        }
+
+        public void runMenu(){
+
+        }
+
+    }
+}
---
+++ src/main/java/week02/Office.java
@@ -0,0 +1,45 @@
+package week02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Office {
+
+    ArrayList<MeetingRoom> meetingRooms = new List<>();
+
+    public void addMeetingRoom(MeetingRoom meetingRoom){
+
+    }
+
+    public void printNames(){
+    }
+
+    public void printNamesReverse(){
+
+    }
+
+    public void printEvenNames(){
+
+    }
+
+    public void printAreas(){
+
+    }
+
+    public void printMeetingRoomsWithName(String name){
+
+    }
+
+    public void printMeetingRoomsContains(String part){
+
+    }
+
+    public void printAreasLargerThan(int area){
+
+    }
+
+
+
+
+
+}
