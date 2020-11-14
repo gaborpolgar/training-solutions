e1f66574b8c7490470f43a402b18fd7043830971
Gábor Polgár <56192725+gaborpolgar@users.noreply.github.com>
2020-11-14 22:06:15 +0100

    packages task commited.

---
+++ src/main/java/packages/main/MainProgram.java
@@ -0,0 +1,13 @@
+package packages.main;
+
+import packages.greetings.Greeter;
+
+public class MainProgram {
+
+    public void main(){
+        Greeter greeter = new Greeter();
+        greeter.sayHello();
+
+
+    }
+}
