d2fd1ae324574e1f2e0593dbbbeba50b02a69aa4
Gábor Polgár <56192725+gaborpolgar@users.noreply.github.com>
2020-11-15 20:09:59 +0100

    lowToHigh.

---
+++ src/main/java/javaapi/Upper.java
@@ -0,0 +1,10 @@
+package javaapi;
+
+public class Upper {
+
+    private String lowercase;
+
+    public String toUpper (String lowerCase){
+        return lowerCase.toUpperCase();
+    }
+}
---
+++ src/main/java/javaapi/mainUpper.java
@@ -0,0 +1,9 @@
+package javaapi;
+
+public class mainUpper {
+
+    public static void main(String[] args) {
+        Upper lowerCaseString = new Upper();
+        System.out.println(lowerCaseString.toUpper("acme"));
+    }
+}
