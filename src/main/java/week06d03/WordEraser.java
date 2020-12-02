097d57c8e6e83f2e7c72779c5918c10a2eb684c4
Gábor Polgár <56192725+gaborpolgar@users.noreply.github.com>
2020-12-02 19:19:35 +0100

    daily exercise fixed!

--- src/main/java/week06d03/WordEraser.java
+++ src/main/java/week06d03/WordEraser.java
@@ -2,19 +2,15 @@ package week06d03;
 
 public class WordEraser {
 
-    public String eraseWord (String words, String word){
+    public String eraseWord(String words, String word) {
         String wordsOfArray[] = words.split(" ");
-        for (String examinedW : wordsOfArray){
-            if(examinedW.contains(word)){
-                examinedW = "";
-            }
-        }
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < wordsOfArray.length; i++) {
-            sb.append(wordsOfArray[i]);
-            sb.append(" ");
-
+            if (!wordsOfArray[i].contains(word)) {
+                sb.append(wordsOfArray[i]);
+                    sb.append(" ");
+            }
         }
-        return sb.toString();
+        return sb.toString().trim();
     }
 }
