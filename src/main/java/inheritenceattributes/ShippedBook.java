36276cace220b2aafcd3ec6a4699678b01b22c61
Gábor Polgár <56192725+gaborpolgar@users.noreply.github.com>
2021-01-04 22:26:32 +0100

    Committed.

---
+++ src/main/java/inheritenceattributes/ShippedBook.java
@@ -0,0 +1,20 @@
+package inheritenceattributes;
+
+public class ShippedBook extends Book {
+
+    private int shippingCost;
+
+    public ShippedBook(String title, int price, int shippingCost) {
+        super(title, price);
+        this.shippingCost = shippingCost;
+    }
+
+    public int order (int pieces){
+        return shippingCost + super.purchase(pieces);
+    }
+
+    @Override
+    public String toString() {
+        return getTitle() + ":" + price + " - " + shippingCost;
+    }
+}
