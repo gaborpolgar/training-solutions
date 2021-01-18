f6ffb9dc19230b9197d7d781cec7a8d533ebd623
Gábor Polgár <56192725+gaborpolgar@users.noreply.github.com>
2021-01-18 23:14:23 +0100

    Committed.

---
+++ src/main/java/ioreadstring/names/FileManager.java
@@ -0,0 +1,41 @@
+package ioreadstring.names;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileManager {
+
+    private Path myFile;
+    private List<Human> humans = new ArrayList<>();
+
+
+    public FileManager(String fileRelativePath) {
+        this.myFile = Path.of(fileRelativePath);
+    }
+
+    public Path getMyFile() {
+        return myFile;
+    }
+
+    public List<Human> getHumans() {
+        return humans;
+    }
+
+    public void readFromFile() {
+        try {
+            List<String> fileInString = Files.readAllLines(myFile);
+            for (String s : fileInString) {
+                String firstAndLast[] = s.split(" ");
+                Human h = new Human(firstAndLast[0], firstAndLast[1]);
+                humans.add(h);
+            }
+        } catch (IOException e) {
+            throw new IllegalStateException("Can't reach the file!", e);
+        }
+
+    }
+}
